09db79d4893ef73e9f35b4f386a7c2e576952bf0
phantxsma <murraykerron@gmail.com>
2023-11-05 13:55:00 -0500

    Completed script.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.newsletter form');
+  const messageDiv = document.querySelector('.message');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const emailInput = form.querySelector('input[name="email"]');
+    const userEmail = emailInput.value;
+    messageDiv.textContent = `Thank you! Your email address "${userEmail}" has been added to our mailing list!`;
+    form.reset();
+  });
+});
